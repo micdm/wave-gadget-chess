fcbd62da3eed1473a30a22a650fbf4924454cb72
Mic <mic-dm@mail.ru>
2013-03-07 01:23:49 +0700

    new: players (part 2)

---
+++ js/player.js
@@ -0,0 +1,53 @@
+var Player = function(color, id, name, avatar) {
+    this._color = color;
+    this._name = name;
+    this._avatar = avatar;
+    this._node = $('.players .' + color);
+};
+
+Player.prototype.is = function(id) {
+    return this._id == id;
+};
+
+Player.prototype.init = function() {
+    this._node.attr('title', this._name);
+    this._node.css('background-image', 'url(' + this._avatar + ')');
+};
+
+var Players = function() {
+    this._list = {};
+    this._isLocked = false;
+    this._color = Piece.COLORS.WHITE;
+};
+
+Players.prototype.has = function(color) {
+    return color in this._list;
+};
+
+Players.prototype.set = function(color, id, name, avatar) {
+    var player = new Player(color, id, name, avatar);
+    player.init();
+    this._list[color] = player;
+};
+
+Players.prototype.lock = function() {
+    this._isLocked = true;
+};
+
+Players.prototype.turn = function() {
+    this._color = (this._color == Piece.COLORS.WHITE) ? Piece.COLORS.BLACK : Piece.COLORS.WHITE;
+    this._isLocked = false;
+};
+
+Players.prototype.canPlay = function(id) {
+    if (!this.has(this._color)) {
+        return true;
+    }
+    if (!this._list[this._color].is(id)) {
+        return false;
+    }
+    if (this._isLocked) {
+        return false;
+    }
+    return true;
+};
